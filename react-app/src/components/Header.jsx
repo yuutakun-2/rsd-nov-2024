a982e64cbe115d3ce45f46c4ccf2164c5f0c6a49
yuutakun-2 <arkarchanmyae2@gmail.com>
2024-12-10 12:20:30 +0630

    Added useLocation feature to detect current url

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,61 +1,64 @@
-import {
-    Box,
-    AppBar,
-    Toolbar,
-    Typography,
-    IconButton,
-} from "@mui/material";
+import { Box, AppBar, Toolbar, Typography, IconButton } from "@mui/material";
 
 import {
-    Menu as MenuIcon,
-    Add as AddIcon,
-    LightMode as LightModeIcon,
-    DarkMode as DarkModeIcon,
+  Menu as MenuIcon,
+  Add as AddIcon,
+  LightMode as LightModeIcon,
+  DarkMode as DarkModeIcon,
+  ArrowBack as ArrowBackIcon,
 } from "@mui/icons-material";
+
+import { useLocation, useNavigate } from "react-router";
 
 import { useApp } from "../AppProvider";
 
 export default function Header() {
-    const { showForm, setShowForm, mode, setMode, setShowDrawer } = useApp();
+  const { showForm, setShowForm, mode, setMode, setShowDrawer } = useApp();
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
 
-	return (
-		<AppBar position="static">
-			<Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-				<Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-                    <IconButton
-                        color="inherit"
-                        onClick={() => setShowDrawer(true)}>
-                        <MenuIcon />
-                    </IconButton>
-					<Typography>App</Typography>
-				</Box>
+  return (
+    <AppBar position="static">
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          {pathname === "/" ? (
+            <IconButton color="inherit" onClick={() => setShowDrawer(true)}>
+              <MenuIcon />
+            </IconButton>
+          ) : (
+            <IconButton color="inherit" onClick={() => navigate("/login")}>
+              <ArrowBackIcon />
+            </IconButton>
+          )}
+          <Typography>App</Typography>
+        </Box>
 
-				<Box sx={{ display: "flex", gap: 1 }}>
-					<IconButton
-						color="inherit"
-						onClick={() => setShowForm(!showForm)}>
-						<AddIcon />
-					</IconButton>
+        <Box sx={{ display: "flex", gap: 1 }}>
+          <IconButton color="inherit" onClick={() => setShowForm(!showForm)}>
+            <AddIcon />
+          </IconButton>
 
-					{mode == "dark" ? (
-						<IconButton
-							color="inherit"
-							onClick={() => {
-								setMode("light");
-							}}>
-							<LightModeIcon />
-						</IconButton>
-					) : (
-						<IconButton
-							color="inherit"
-							onClick={() => {
-								setMode("dark");
-							}}>
-							<DarkModeIcon />
-						</IconButton>
-					)}
-				</Box>
-			</Toolbar>
-		</AppBar>
-	);
+          {mode == "dark" ? (
+            <IconButton
+              color="inherit"
+              onClick={() => {
+                setMode("light");
+              }}
+            >
+              <LightModeIcon />
+            </IconButton>
+          ) : (
+            <IconButton
+              color="inherit"
+              onClick={() => {
+                setMode("dark");
+              }}
+            >
+              <DarkModeIcon />
+            </IconButton>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
