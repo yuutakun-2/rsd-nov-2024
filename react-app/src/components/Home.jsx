fc2f44856d76b1bee302022348db5b1bf96e1788
yuutakun-2 <arkarchanmyae2@gmail.com>
2024-12-14 14:58:58 +0630

    Actually added remove function

--- react-app/src/components/Home.jsx
+++ react-app/src/components/Home.jsx
@@ -12,10 +12,32 @@ async function fetchPosts() {
   return res.json();
 }
 
+async function deletePost(id) {
+  const res = await fetch(`${api}/${id}`, { method: "DELETE" });
+  return res.json();
+}
+
 export default function Home() {
   const queryClient = useQueryClient();
   const { add, showForm } = useApp();
   const { data, error, isLoading } = useQuery("posts", fetchPosts);
+
+  // useMutation accepts two parameters:
+  // first is a function reference which will connect with the API to actually operate the data into the backend
+  // second is the function that will run before the mutation function (first function) runs
+  const remove = useMutation(deletePost, {
+    onMutate: (id) => {
+      // write the code to change just the ui here
+      // first the cache data should be updated (give cache key defined in useQuery above)
+      queryClient.setQueryData("posts", (old) => {
+        return old.filter((post) => {
+          return post.id != id;
+        });
+      });
+      // old refers to the old cache data, which is all posts within the database
+      // in old.filter(post), post refers to each post
+    },
+  });
 
   // တချို့နေရာတွေမှာ Data မရောက်သေးဘဲ Component ကို အသုံးပြုမိရင် Error တက်နိုင်ပါတယ်။ ဒါကြောင့် ဒီပြဿနာကို ထည့်ဖြေရှင်းဖို့လိုပါတယ်။ ဒါကြောင့် Home.jsx ကို အခုလိုပြင်လိုက်ပါ။
   // if (error) {
@@ -30,7 +52,10 @@ export default function Home() {
     <div>
       {showForm && <Form add={add} />}
 
-      {data && data.map((post) => <Item key={post.id} post={post} />)}
+      {data &&
+        data.map((post) => (
+          <Item key={post.id} post={post} remove={remove.mutate} />
+        ))}
     </div>
   );
 }
