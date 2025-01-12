42009dfc2f51724c4440517e86f120c8119f1de5
Ei Maung <eimg@fairwayweb.com>
2025-01-12 17:03:26 +0630

    next

---
+++ next-app/app/person/[id]/page.tsx
@@ -0,0 +1,12 @@
+export default async function Person({
+	params,
+}: {
+	params: Promise<{ id: string }>;
+}) {
+	return (
+		<div>
+			<h2 className="pb-2 mb-4 border-b font-bold text-lg">Person</h2>
+			{(await params).id}
+		</div>
+	);
+}
---
+++ next-app/app/search/page.tsx
@@ -0,0 +1,7 @@
+export default function Search() {
+	return (
+		<div>
+			<h2 className="pb-2 mb-4 border-b font-bold text-lg">Search</h2>
+		</div>
+	);
+}
